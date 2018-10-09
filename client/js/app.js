0dd0d158481591bcf1d028b67d94264171798172
emsou <mikhail.soumar@gmail.com>
2018-10-09 18:01:13 -0400

    Added persistent storage with remote (mLab) database

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,23 @@
 var app=angular.module('myApp',[]);
 
 app.controller('mainController',['$scope',function($scope){
- var socket = io.connect();
- $scope.send = function(){
-  socket.emit('chat message', $scope.message);
-  $scope.message="";
- }
- socket.on('chat message', function(msg){
-  var li=document.createElement("li");
-  li.appendChild(document.createTextNode(msg));
-  document.getElementById("messages").appendChild(li);
- });
+    var socket = io.connect();
+    $scope.send = function(){
+        socket.emit('chat message', $scope.message);
+        $scope.message="";
+    }
+    socket.on('load messages', function(docs){
+        for(var i = docs.length-1; i >= 0; i--){
+            displayMsg(docs[i].message);
+        }
+    });
+    socket.on('chat message', function(msg){
+        displayMsg(msg);
+    });
+
+    function displayMsg(msg){
+        var li=document.createElement("li");
+        li.appendChild(document.createTextNode(msg));
+        document.getElementById("messages").appendChild(li);
+    }
 }]);
--- server/server.js
+++ server/server.js
@@ -3,20 +3,52 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//mongoDB
+var mongoose = require('mongoose');
+var url = 'mongodb://jim:m3tokur@ds127293.mlab.com:27293/tangent_db';
+mongoose.connect(url, {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to remote database at ' + url);
+});
+
+//message schema
+var testSchema = new mongoose.Schema({
+    name: String,
+    message: String,
+    timestamp: {type: Date, default: Date.now}
+});
+var Message = mongoose.model('Message', testSchema);
+
+//app
 app.use('/', express.static(__dirname));
 app.use('/', express.static(__dirname + '/../client/'));
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
-  console.log('user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    console.log('user connected');
+
+    var query =  Message.find({});
+    query.sort('-timestamp').limit(100).exec(function(err, docs){
+        if(err) throw err;
+        socket.emit('load messages', docs);
+    });
+    
+    socket.on('chat message', function(msg){
+        var msgDoc = new Message({message: msg, name: 'Anonymous'});
+        msgDoc.save(function(err){
+            if(err) throw err;
+            io.emit('chat message', msg);
+        });
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
